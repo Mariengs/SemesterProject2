f9bf3b1e9f56f903254eb7e80aebbbe13b26b9a9
Mariengs <marianne.waago@hotmail.com>
2025-04-10 13:01:32 +0200

    tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js}",
+    "./js/**/*.{js}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
