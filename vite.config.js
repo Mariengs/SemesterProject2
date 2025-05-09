a7dd69a99004dc6a33b3e563885451c660513e6b
Mariengs <marianne.waago@hotmail.com>
2025-05-09 11:44:11 +0200

    vite?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  // Ingen spesielle plugins nødvendige for Tailwind, men du kan legge til eventuelle andre du trenger her
-});
+export default defineConfig({});
